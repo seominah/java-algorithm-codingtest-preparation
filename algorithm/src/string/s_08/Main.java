88a9b8bba072676797dbd03f5406a5d9fd439f3c
DESKTOP-C5JTEQC\SMK <seominah95@gmail.com>
2022-05-11 00:12:01 +0900

    팰린드룸, *

---
+++ algorithm/src/string/s_08/Main.java
@@ -0,0 +1,22 @@
+package string.s_08;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) {
+            answer="YES";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
